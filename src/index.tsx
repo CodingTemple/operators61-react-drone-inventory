72c12b00700b5aad35fc6c952894e35f3ca8d3e7
nwelter1 <nwelter1@gmail.com>
2021-05-24 21:07:49 -0500

    Creating our first Component

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Home } from './components'
+import './styles.css'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Home title='Operators-61 Drone Inventory'/>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -15,3 +15,6 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
